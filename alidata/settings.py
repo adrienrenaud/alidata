0e9cbc3ca771a37e1da12ad9004a5c49609e1a62
arenaud <adrien.renaud@gmail.com>
2016-05-13 20:53:20 -0700

    foo

--- alidata/settings.py
+++ alidata/settings.py
@@ -24,9 +24,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
