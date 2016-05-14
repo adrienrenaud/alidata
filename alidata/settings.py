a2f6ae4cc88a48badc0c1a5274cf2e80ba167a68
arenaud <adrien.renaud@gmail.com>
2016-05-14 11:19:53 -0700

    bar

--- alidata/settings.py
+++ alidata/settings.py
@@ -167,13 +167,13 @@ STATIC_URL = '/static/'
 
 
 
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
+# # List of finder classes that know how to find static files in
+# # various locations.
+# STATICFILES_FINDERS = (
+    # 'django.contrib.staticfiles.finders.FileSystemFinder',
+    # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+# )
 
 
 
